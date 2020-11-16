701e26e0c9d3d49250ff9b77431dc485fc1d9c60
eilon299 <57979428+eilon299@users.noreply.github.com>
2020-11-16 12:41:14 +0200

    exc 10

--- problems/map_problem.py
+++ problems/map_problem.py
@@ -65,8 +65,9 @@ class MapProblem(GraphProblem):
         #        `link.distance`). You don't have to specify the operator name here.
         #  Note: Generally, in order to check whether a variable is set to None you should use the expression:
         #        `my_variable_to_check is None`, and particularly do NOT use comparison (==).
-
-        yield OperatorResult(successor_state=MapState(self.target_junction_id), operator_cost=7)  # TODO: remove this line!
+        for i in junction.outgoing_links:
+            yield OperatorResult(successor_state=MapState(i.target),
+                                 operator_cost=i.distance)  # TODO: remove this line!
 
     def is_goal(self, state: GraphProblemState) -> bool:
         """
@@ -74,6 +75,6 @@ class MapProblem(GraphProblem):
         """
         assert (isinstance(state, MapState))
 
-        # TODO [Ex.10]: modify the returned value to indicate whether `state` is a final state.
+        # TODO [Ex.10]: modify the returned value to indicate whether `state` is a final state. - done!
         # You may use the problem's input parameters (stored as fields of this object by the constructor).
-        return state.junction_id == 14593  # TODO: modify this!
+        return state.junction_id == self.target_junction_id  # TODO: modify this!
